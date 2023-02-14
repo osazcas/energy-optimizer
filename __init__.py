e17991d6e9ebdb0988ffbabcff80eae2ec4962d7
osazcas <oscar.azo1@gmail.com>
2023-02-14 18:14:34 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Energy Optimizer."""
+
+from .version import VERSION
+
+__author__ = "Oscar Azofeifa"
+__email__ = "oscar.azo1@gmail.com"
+__version__ = VERSION
+
+__all__ = ["__author__", "__email__", "__version__"]
